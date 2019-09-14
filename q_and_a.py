4a8795f764056d88ec17f80ee83c7ac1283bb52e
Stewy Slocum <sslocum3@jhu.edu>
2019-09-14 14:35:02 -0400

    minor changes

---
+++ q_and_a.py
@@ -0,0 +1,22 @@
+
+
+def q_and_a():
+    q_and_a = [
+        {'question':
+         '''What is the name of:
+         The number of protons of each atom in the element?''',
+
+         'answers': ['atomic number']},
+
+        {'question':
+         '''6.02 times 10 to the 23rd particles is equal to:''',
+
+         'answers': ['mole']},
+
+        {'question':
+         '''What is the name for the transition from a solid to a gas?''',
+
+         'answers': ['sublimation']}
+    ]
+
+    return 'Hi'
