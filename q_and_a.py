13071750ccd1ef8b658b785091c72080112b33f4
Stewy Slocum <sslocum3@jhu.edu>
2019-09-14 22:17:19 -0400

    rolled back to working version

--- q_and_a.py
+++ q_and_a.py
@@ -1,63 +1,34 @@
 import random
-import gensim.downloader
 import pprint
 
 class QandAHandler:
 
 
-    def __init__(self, model=None):
-        self.q_and_ks = [
-            {'question':
-             '6.02 times 10 to the 23rd particles is equal to:',
-
-             'keywords': ['mole']},
-
-            {'question':
-             '''What is the name for the transition from a solid to a gas?''',
-
-             'keywords': ['sublimation']},
-
+    def __init__(self):
+        self.q_and_as = [
             {'question':
              'How are photosystems involved in photosynthesis?',
 
-             'keywords': ['chlorophyll', 'absorption', 'photon', 'excite']}
+             'answer': 'Photons excite chlorophyll which produces energy.'}
         ]
 
-        self.last_q_and_ks = None
-
-        self.model = model
-        if self.model is None:
-            pass
-            #self.model = gensim.downloader.load('glove-twitter-25')
+        self.last_q_and_a = None
 
 
     def supply_question(self):
-        self.last_q_and_ks = random.choice(self.q_and_ks)
-        return self.last_q_and_ks['question']
+        self.last_q_and_a = random.choice(self.q_and_as)
+        return self.last_q_and_a['question']
 
 
     def evaluate_response(self, response):
-        keywords = self.last_q_and_ks['keywords']
+        answer = self.last_q_and_a['answer']
 
-        score = 0
-        for kw in keywords:
-            #if kw not in self.model.wv.vocab:
-            #    break
+        score = 1
 
-            kw_match = 0
-            for rw in response.split(' '):
-                #if rw not in self.model.wv.vocab:
-                #    break
-
-                kw_match = max(kw_match, kw == rw)#self.model.wv.similarity(kw, rw))
-
-            print(f'keyword: {kw}, response: {response}, score: {kw_match}')
-            score += kw_match
-
-        if score / len(keywords) > 0.9:
+        if score > 0.9:
             return 'You got it - score: {score}'
 
-        return f'Sorry, the correct answer is: {self.last_q_and_ks["keywords"][0]} - score: {score}'
+        return f'Sorry, the correct answer is: {answer} - score: {score}'
 
 
 def main():
