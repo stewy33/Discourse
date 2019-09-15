bb35735a6b45d252902e9bec097c2eb1793be161
Stewy Slocum <sslocum3@jhu.edu>
2019-09-14 22:37:35 -0400

    jlkj

--- q_and_a.py
+++ q_and_a.py
@@ -1,5 +1,9 @@
 import random
 import pprint
+import requests
+import json
+import numpy as np
+from sklearn.preprocessing import normalize
 
 class QandAHandler:
 
@@ -23,10 +27,16 @@ class QandAHandler:
     def evaluate_response(self, response):
         answer = self.last_q_and_a['answer']
 
-        score = 1
+        url = 'https://api.msturing.org/gen/encode'
+        headers = {'Ocp-Apim-Subscription-Key': '04414dcfaf7c43a4b918e5c14a8bfd0c'}
+        payload = json.dumps({'queries': [response, answer]})
+
+        req = requests.post(url, headers=headers, data=payload)
+        vec1, vec2 = [np.array(q['vector']) for q in req.json()]
+        score = np.dot(vec1, vec2)
 
         if score > 0.9:
-            return 'You got it - score: {score}'
+            return f'You got it - score: {score}'
 
         return f'Sorry, the correct answer is: {answer} - score: {score}'
 
