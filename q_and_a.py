6f9b5408412b69d7b9ebbb99c6ca2bf732ef7416
Stewy Slocum <sslocum3@jhu.edu>
2019-09-15 02:34:53 -0400

    using fasttext

--- q_and_a.py
+++ q_and_a.py
@@ -4,19 +4,59 @@ import requests
 import json
 import numpy as np
 
+def read_word_vecs():
+    with open('../wiki-news-300d-1M-subword.vec', 'r', encoding='utf-8',
+              newline='\n', errors='ignore') as fin:
+        _, _ = map(int, fin.readline().split())
+
+        word_vecs = {}
+        for line in fin:
+            tokens = line.rstrip().split(' ')
+            vec = np.array(list(map(float, tokens[1:])))
+            word_vecs[tokens[0]] = vec / np.linalg.norm(vec)
+
+    return word_vecs
+
+
 class QandAHandler:
 
 
-    def __init__(self):
+    def __init__(self, word_vecs=None):
         self.q_and_as = [
-            {'question': 'What kind of fluid is normal saline?',
-             'keywords': ['crystalloid fluid']},
+            {'question': 'What does it mean for a problem to be in class p?',
+             'keywords': [['polynomial']],
+             'answer': '''If a problem is in class p, there exists a polynomial
+time algorithm to solve it.'''},
 
-            {'question': 'What is the function of chlorophyll?',
-             'keywords': ['photosynthesis', 'energy', 'glucose']}
+            {'question': 'What is a classifier?',
+             'keywords': [['class', 'category']],
+             'answer': '''A classifier classifies inputs into distinct
+categories.'''},
+
+            {'question': 'What is a hash table?',
+             'keywords': [['hash', 'map'], ['key', 'index']],
+             'answer': '''A hash table is a data structure that hashes keys to
+integers which are used to efficiently index an internal array.'''},
+
+            {'question': 'What is a binary tree?',
+             'keywords': [['two'], ['children', 'subtree']],
+             'answer': '''A binary tree is a tree where every node has at most
+two children.'''}
         ]
 
         self.last_q_and_a = None
+
+        if word_vecs:
+            self.word_vecs = word_vecs
+        else:
+            self.word_vecs = read_word_vecs()
+
+
+    def similarity(self, w1, w2):
+        if w1 not in self.word_vecs or w2 not in self.word_vecs:
+            return 0
+
+        return np.dot(self.word_vecs[w1], self.word_vecs[w2])
 
 
     def supply_question(self):
@@ -26,49 +66,21 @@ class QandAHandler:
 
 
     def evaluate_response(self, response):
-        question = self.last_q_and_a['question']
         keywords = self.last_q_and_a['keywords']
 
-        return 'response not evaluated yet'
-
-        url = 'https://api.msturing.org/gen/encode'
-        headers = {'Ocp-Apim-Subscription-Key':
-                   '04414dcfaf7c43a4b918e5c14a8bfd0c'}
-
-        payload = json.dumps({'queries': keywords})
-
-        req_k = requests.post(url, headers=headers, data=payload).json()
-        print(req_k)
-        for i in range(len(req_k)):
-            q = req_k[i]
-            v = np.array(q['vector'])
-            req_k[i]['vector'] = v / np.linalg.norm(v)
-
-
-        response_grams = []
-        qsplit = question.split(' ')
-        for i in range(0, len(qsplit) - 4, 2):
-            response_grams.append(' '.join(qsplit[i:i+4]))
-        payload = json.dumps({'queries': response_grams})
-
-        req_r = requests.post(url, headers=headers, data=payload).json()
-        for i in range(len(req_r)):
-            q = req_r[i]
-            v = np.array(q['vector'])
-            req_r[i]['vector'] = v / np.linalg.norm(v)
-
         scores = []
-        for q_k in req_k:
+        for kw_group in keywords:
             top_score = -1
-            for q_r in req_r:
-                top_score = max(top_score, np.dot(q_k['vector'], q_r['vector']))
+
+            for kw in kw_group:
+                for rw in response.lower().split():
+                    top_score = max(top_score, self.similarity(kw, rw))
+
             scores.append(top_score)
 
-
-        print(scores)
         score = sum(scores) / len(scores)
 
-        if score > 0.7:
+        if score > 0.75:
             return f'You got it - score: {score}'
 
         return f'Sorry, the correct answer is: {keywords} - score: {score}'
